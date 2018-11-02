c3c5512f0e282b0f9758916d65acdd8ea53ef780
Hadrien Lanneau <hadrien@lanneau.me>
2018-11-02 17:19:13 +0100

    Advanced tests with enzyme

--- src/views/List/List.test.js
+++ src/views/List/List.test.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import List from './';
+import { getAll } from '../../services/api';
+
+jest.mock('../../services/api', () => ({
+  getAll: jest.fn(() => []),
+}));
 
 it('should render', () => {
   const tree = renderer.create(<List />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('should load all items on mount', () => {
+  shallow(<List />);
+  expect(getAll).toHaveBeenCalled();
+})
+
+it('should list items', () => {
+  const wrapper = shallow(<List />);
+  const items = [{id: 1}, {id: 2}, {id: 3}];
+  wrapper.setState({ items });
+  expect(wrapper.find('Item').length).toBe(3)
+})
+
+
+it('should remove item', () => {
+  const wrapper = shallow(<List />);
+  const items = [{id: 1}, {id: 2}, {id: 3}];
+  wrapper.setState({ items });
+  wrapper.instance().onRemove(2);
+  expect(wrapper.state().items.length).toBe(2);
+  expect(wrapper.state().items[0].id).toBe(1);
+  expect(wrapper.state().items[1].id).toBe(3);
+})
