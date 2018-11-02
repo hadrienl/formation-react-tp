16105d799962212137991cf13497c34beb1b3cad
Hadrien Lanneau <hadrien@lanneau.me>
2018-11-02 17:25:23 +0100

    Configure storybook with knobs

---
+++ src/stories/exemple.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+
+const stories = storiesOf('Storybook Knobs', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('with a button', () => (
+  <button disabled={boolean('Disabled', false)} >
+    {text('Label', 'Hello Storybook')}
+  </button>
+));
+
+// Knobs as dynamic variables.
+stories.add('as dynamic variables', () => {
+  const name = text('Name', 'Arunoda Susiripala');
+  const age = number('Age', 89);
+
+  const content = `I am ${name} and I'm ${age} years old.`;
+  return (<div>{content}</div>);
+});
